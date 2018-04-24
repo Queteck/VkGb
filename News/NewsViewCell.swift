e1e6ec0e0ac0d960bb084bc88e76d6227c513f67
Vitaliy Filippov <queteck89@gmail.com>
2018-04-24 14:30:49 +0300

    Add newsfeed

---
+++ News/NewsViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NewsViewCell.swift
+//  VkGb
+//
+//  Created by VitaliyFilippov on 24.04.2018.
+//  Copyright © 2018 VitaliyFilippov. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewCell: UITableViewCell {
+    
+    @IBOutlet weak var AuthorName: UILabel!
+    @IBOutlet weak var AuthorAvatar: UIImageView!
+    @IBOutlet weak var NewsImage: UIImageView!
+    @IBOutlet weak var NewsText: UILabel!
+    @IBOutlet weak var Like: UIImageView!
+    @IBOutlet weak var LikesCount: UILabel!
+    @IBOutlet weak var Comment: UIImageView!
+    @IBOutlet weak var CommentsCount: UILabel!
+    @IBOutlet weak var Repost: UIImageView!
+    @IBOutlet weak var RepostCount: UILabel!
+    @IBOutlet weak var Views: UIImageView!
+    @IBOutlet weak var ViewsCount: UILabel!
+    
+    
+
+}
