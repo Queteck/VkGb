38e3f4a983bdddaf3d96a28f0a5589a81c8e0de6
Vitaliy Filippov <queteck89@gmail.com>
2018-05-11 16:26:28 +0300

    add layout and white background fo the labels

--- Friends/FriendsViewCell.swift
+++ Friends/FriendsViewCell.swift
@@ -9,19 +9,68 @@
 import UIKit
 
 class FriendsViewCell: UITableViewCell {
-    @IBOutlet weak var friendsList: FriendsViewCell!
-    @IBOutlet weak var friendsName: UILabel!
-    @IBOutlet weak var friendAvatar: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let instets: CGFloat = 10.0
+    
+    @IBOutlet weak var friendsList: FriendsViewCell!{
+        didSet {
+            friendsList.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBOutlet weak var friendsName: UILabel!{
+        didSet {
+            friendsName.translatesAutoresizingMaskIntoConstraints = false
+        }
     }
-
+    @IBOutlet weak var friendAvatar: UIImageView!{
+        didSet {
+            friendAvatar.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    func setFriendsName(text: String) {
+        friendsName.text = text
+        friendsNameLabelFrame()
+    }
+    
+    func getLabelSize(text: String, font: UIFont) -> CGSize {
+        // определяем максимальную ширину текста - это ширина ячейки минус отступы слева и справа
+        let maxWidth = bounds.width - instets * 2
+        // получаем размеры блока под надпись
+        // используем максимальную ширину и максимально возможную высоту
+        let textBlock = CGSize(width: maxWidth, height: CGFloat.greatestFiniteMagnitude)
+        // получаем прямоугольник под текст в этом блоке и уточняем шрифт
+        let rect = text.boundingRect(with: textBlock, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        // получаем ширину блока, переводим ее в Double
+        let width = Double(rect.size.width)
+        // получаем высоту блока, переводим ее в Double
+        let height = Double(rect.size.height)
+        // получаем размер, при этом округляем значения до большего целого числа
+        let size = CGSize(width: ceil(width), height: ceil(height))
+        return size
+    }
+    
+    func friendsNameLabelFrame() {
+        // получаем размер текста, передавая сам текст и шрифт
+        let friendsNameLabelSize = getLabelSize(text: friendsName.text!, font: friendsName.font)
+        // рассчитываем координату по оси Х
+        let friendsNameLabelX = (bounds.width - friendsNameLabelSize.width) / 2
+        // получаем точку верхнего левого угла надписи
+        let friendsNameLabelOrigin = CGPoint(x: friendsNameLabelX, y: instets)
+        // получаем фрейм и устанавливаем его UILabel
+        friendsName.frame = CGRect(origin: friendsNameLabelOrigin, size: friendsNameLabelSize)
+    }
+    
+    func friendAvatarFrame() {
+        let friendAvatarSideLinght: CGFloat = 50
+        let friendAvatarSize = CGSize(width: friendAvatarSideLinght, height: friendAvatarSideLinght)
+        let friendAvatarOrigin = CGPoint(x: bounds.midX - friendAvatarSideLinght / 2, y: bounds.midY - friendAvatarSideLinght / 2)
+        friendAvatar.frame = CGRect(origin: friendAvatarOrigin, size: friendAvatarSize)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        friendAvatarFrame()
+        friendsNameLabelFrame()
+    }
 }
--- FriendsPhoto/FriendsPhotoViewCell.swift
+++ FriendsPhoto/FriendsPhotoViewCell.swift
@@ -9,6 +9,26 @@
 import UIKit
 
 class FriendsPhotoViewCell: UICollectionViewCell {
-    @IBOutlet weak var photoView: UIImageView!
     
+    let instets: CGFloat = 10.0
+    
+    @IBOutlet weak var photoView: UIImageView!{
+        didSet {
+            photoView.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    func iconFrame() {
+        let iconSideLinght: CGFloat = 50
+        let iconSize = CGSize(width: iconSideLinght, height:
+            iconSideLinght)
+        let iconOrigin = CGPoint(x: bounds.midX - iconSideLinght / 2, y:
+            bounds.midY - iconSideLinght / 2)
+        photoView.frame = CGRect(origin: iconOrigin, size: iconSize)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        iconFrame()
+    }
 }
