6145ee6639114fac9dd97acf1dcce90e66fd7562
Vitaliy Filippov <queteck89@gmail.com>
2018-06-05 17:50:13 +0300

    Add map kit

---
+++ AddPost/MapKitViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapKitViewController.swift
+//  VkGb
+//
+//  Created by VitaliyFilippov on 05.06.2018.
+//  Copyright © 2018 VitaliyFilippov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapKitViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBAction func closeMap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
