6e2a57165cb210763b85e53d358ffcb4cef1715a
Vitaliy Filippov <queteck89@gmail.com>
2018-07-12 20:23:32 +0300

    Add Mapkit

--- AddPost/MapKitViewController.swift
+++ AddPost/MapKitViewController.swift
@@ -15,21 +15,9 @@ class MapKitViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
-        if let currentLocation = locations.last?.coordinate {
-            print(currentLocation)
-            let coordinate = CLLocation(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
-            let coder = CLGeocoder()
-            coder.reverseGeocodeLocation(coordinate) {(myPlaces,Error) -> Void in
-                if let place = myPlaces?.first {
-                    print(place.locality)
-                }
-            }
-            let currentRadius: CLLocationDistance = 1000
-            let currentRegion = MKCoordinateRegionMakeWithDistance((currentLocation), currentRadius *
-                2.0, currentRadius * 2.0)
-            self.mapView.setRegion(currentRegion, animated: true)
-            self.mapView.showsUserLocation = true
-        }
+        guard let coordinate = locations.last else { return }
+        zoomIn(coordinate: coordinate)
+        
     }
     @IBAction func closeMap(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -37,26 +25,27 @@ class MapKitViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        if let coordinate = locationManager.location {
+            zoomIn(coordinate: coordinate)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func zoomIn(coordinate: CLLocation) {
+        let coder = CLGeocoder()
+        coder.reverseGeocodeLocation(coordinate) {(myPlaces,Error) -> Void in
+            if let place = myPlaces?.first {
+                print(place.locality)
+            }
+        }
+        let currentRadius: CLLocationDistance = 1000
+        let currentRegion = MKCoordinateRegionMakeWithDistance((coordinate.coordinate), currentRadius *
+            2.0, currentRadius * 2.0)
+        self.mapView.setRegion(currentRegion, animated: true)
+        self.mapView.showsUserLocation = true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
